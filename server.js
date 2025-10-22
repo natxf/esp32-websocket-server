efd323ce9aac09eb9acd08ced10b8b5a02a01ec7
natxf <157922167+natxf@users.noreply.github.com>
2025-10-22 22:43:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,27 +10,36 @@ let appSocket = null;
 wss.on('connection', (ws) => {
   console.log('New client connected.');
 
-ws.on('message', (message) => {
-  const msg = message.toString('utf8');
-  console.log('Received:', msg);
+  ws.on('message', (message) => {
+    const msg = message.toString('utf8');
+    console.log('Received:', msg);
 
-  if (msg === 'ESP') {
-    espSocket = ws;
-    console.log('Registered ESP device.');
-  } else if (msg === 'APP') {
-    appSocket = ws;
-    console.log('Registered Flutter app.');
-  } else if (ws === appSocket && espSocket && espSocket.readyState === WebSocket.OPEN) {
-    espSocket.send(msg);
-    console.log('Forwarded to ESP:', msg);
-  } else if (ws === espSocket) {
-    // Accept messages even if no app is connected yet
-    console.log('Received from ESP (no app connected yet):', msg);
-  } else {
-    console.log('Unhandled message or no valid peer connected.');
-  }
-});
-
+    if (msg === 'ESP') {
+      espSocket = ws;
+      console.log('Registered ESP device.');
+    } else if (msg === 'APP') {
+      appSocket = ws;
+      console.log('Registered Flutter app.');
+    } else if (ws === espSocket) {
+      // ESP sent something
+      if (appSocket && appSocket.readyState === WebSocket.OPEN) {
+        appSocket.send(msg);
+        console.log('Forwarded to APP:', msg);
+      } else {
+        console.log('ESP sent a message but APP not connected yet.');
+      }
+    } else if (ws === appSocket) {
+      // APP sent something
+      if (espSocket && espSocket.readyState === WebSocket.OPEN) {
+        espSocket.send(msg);
+        console.log('Forwarded to ESP:', msg);
+      } else {
+        console.log('APP sent a message but ESP not connected yet.');
+      }
+    } else {
+      console.log('Unhandled message or unknown client.');
+    }
+  });
 
   ws.on('close', () => {
     if (ws === espSocket) {
@@ -44,6 +53,7 @@ ws.on('message', (message) => {
     }
   });
 });
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
